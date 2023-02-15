9c94bbf139dc0f23736caef80153aad0b55a4e03
Niles Brandon <niles@koifishcoding>
2023-02-14 22:40:39 -0600

    main: leave mobile menu animation

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -9,9 +9,9 @@
                     <span>Full Stack</span>
                     <span>Developer Program</span>
                 </NuxtLink>
-                <div class="item">Instructor</div>
-                <div class="item">Nashville</div>
-                <div class="item">FAQ</div>
+                <div class="item instructor">Instructor</div>
+                <div class="item nashville">Nashville</div>
+                <div class="item faq">FAQ</div>
                 <div class="item break"></div>
                 <div class="item enroll">Enroll</div>
                 <div class="item login">Student Login</div>
@@ -32,6 +32,7 @@ const props = defineProps<{
 watch(
     () => props.visible,
     (currentVal) => {
+
         if (currentVal) {
             document.body.classList.add('modal-open')
         } else {
@@ -62,8 +63,6 @@ watch(
     align-items: center
 
     .cross
-        // transform: translateX(0)
-        // opacity: 1
         position: absolute
         top: 1rem
         right: 1rem
@@ -72,8 +71,6 @@ watch(
         padding: .4rem 1rem
         border-radius: 50%
         cursor: pointer
-        // transform: rotateZ(0deg)
-        // opacity: 1
 
         @media screen and (min-width: $breakpoint-small)
             &:hover
@@ -119,7 +116,7 @@ watch(
 .mobile-menu-enter-active
     transition: all .3s cubic-bezier(.4, 0, .2, 1)
 .mobile-menu-leave-active
-    transition: all .3s cubic-bezier(.4, 0, .2, 1) .6s
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .7s
 .mobile-menu-enter-from, .mobile-menu-leave-to
     opacity: 0
     backdrop-filter: blur(0)
@@ -127,16 +124,52 @@ watch(
 .mobile-menu-enter-active .cross
     transition: all .3s cubic-bezier(.4, 0, .2, 1) .3s
 .mobile-menu-leave-active .cross
-    transition: all .3s cubic-bezier(.4, 0, .2, 1) .3s
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .7s
 .mobile-menu-enter-from .cross, .mobile-menu-leave-to .cross
     transform: rotateZ(45deg)
     opacity: 0
 
 .mobile-menu-enter-active .developer
-    transition: all .3s cubic-bezier(.4, 0, .2, 1) .6s
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .3s
 .mobile-menu-leave-active .developer
-    transition: all .3s cubic-bezier(.4, 0, .2, 1)
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .6s
 .mobile-menu-enter-from .developer, .mobile-menu-leave-to .developer
+    opacity: 0 !important
+.mobile-menu-enter-active .instructor
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .4s
+.mobile-menu-leave-active .instructor
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .5s
+.mobile-menu-enter-from .instructor, .mobile-menu-leave-to .instructor
+    opacity: 0 !important
+.mobile-menu-enter-active .nashville
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .5s
+.mobile-menu-leave-active .nashville
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .4s
+.mobile-menu-enter-from .nashville, .mobile-menu-leave-to .nashville
+    opacity: 0 !important
+.mobile-menu-enter-active .faq
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .6s
+.mobile-menu-leave-active .faq
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .3s
+.mobile-menu-enter-from .faq, .mobile-menu-leave-to .faq
+    opacity: 0 !important
+.mobile-menu-enter-active .break
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .7s
+.mobile-menu-leave-active .break
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .2s
+.mobile-menu-enter-from .break, .mobile-menu-leave-to .break
+    opacity: 0 !important
+.mobile-menu-enter-active .enroll
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .8s
+.mobile-menu-leave-active .enroll
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .1s
+.mobile-menu-enter-from .enroll, .mobile-menu-leave-to .enroll
+    opacity: 0 !important
+.mobile-menu-enter-active .login
+    transition: all .3s cubic-bezier(.4, 0, .2, 1) .9s
+.mobile-menu-leave-active .login
+    transition: all .3s cubic-bezier(.4, 0, .2, 1)
+.mobile-menu-enter-from .login, .mobile-menu-leave-to .login
     opacity: 0 !important
 
 </style>
